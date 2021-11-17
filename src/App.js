fecea6f17dc37e98ceeb79ebb889e7b5a6173088
prasanth <prasanthyadav549@gmail.com>
2021-11-17 14:13:38 +0530

    app.js is changed

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import './App.css';
+import img from './images/net.jpg';
 
-// eslint-disable-next-line no-unused-vars
-import  img from './images/net.jpg';
+
 
 // eslint-disable-next-line no-unused-vars
 import {Navbar ,Body,Footer,Background} from './components';
